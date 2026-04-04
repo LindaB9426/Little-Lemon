3229f425b4fd59e46470f53342d544263a883f19
LindaB9426 <babre.linda@inbox.lv>
2026-04-04 15:13:02 +0300

    About page is upgrade

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,20 +1,48 @@
 import React from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import A from "../Images/A.jpg";
 
 const About = () => {
   return (
     <Box
       id="about-section"
-      minH="100vh"
-      pt="120px"   
+      minH="auto"
+      pt="10px"
       bg="gray.100"
+      px={8}
+      backgroundColor="white"
+      scrollMarginTop="80px"
+      justifyContent="center"
+      
+     
     >
-      <Heading size="xl" textAlign="center">
-        Hello everybody!
-      </Heading>
-      <p>
-        Let's go on!
-      </p>
+      <Flex
+        
+        height="600px"  
+        bg="#495E57" 
+      >
+      
+        <Box flex="1" mt="20" marginLeft="200px" p={8} flexDirection="column" justifyContent="center" >
+         <Heading size="xl" textAlign="center" mb={8} color="white">
+            Hello everybody!
+          </Heading> 
+          <Text color="white">
+            We are a family owned Mediterranean restaurant, focused on traditional recipes served with modern twist
+          </Text>
+        </Box>
+        <Box flex="2" paddingLeft="200px" >
+          <Image
+          src={A}
+          alt="About image"
+          width="90%"
+          height="60%"
+          objectFit="cover"
+          px="20"
+          mt="20"
+          borderRadius="20%"
+          />
+        </Box>
+      </Flex>
     </Box>
   );
 };
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,11 +9,10 @@ const Footer = () => {
         <Flex
         maxW="1200px"
         mx="auto"
+        px={6}
+        py={10}
         justify="space-between"
-        align="flex-start"
-        flexWrap="wrap"
-        gap={3}
-        p={4}
+        gap={10}
         
         > 
           <Box>
@@ -22,12 +21,12 @@ const Footer = () => {
   
           <Box>
             <Heading size="xs">Contact info</Heading>  
-            <Text fontSize="sm"> Telephone +37025698578</Text>
-            <Text fontSize="sm">E-mail littlelemon@gmail.com</Text>
+            <Text fontSize="xs"> Telephone +37025698578</Text>
+            <Text fontSize="xs">E-mail littlelemon@gmail.com</Text>
           </Box>
           <Box>
             <Heading size="m">Links</Heading>
-            <Flex direction="column" gap={1} fontSize="sm">
+            <Flex direction="column"  fontSize="xs">
               <Link to="/">Home</Link>
               <Link to="/about">About</Link>
               <Link to="/reservation">Reservation</Link>
@@ -37,7 +36,7 @@ const Footer = () => {
           </Box>
           <Box>
             <Heading size="m">Contacts</Heading>
-            <Flex direction="column" gap={1} fontSize="sm">
+            <Flex direction="column"  fontSize="xs">
               <Link to="#">Facebook</Link>
               <Link to="#">Instagram</Link>
               <Link to="#">LinkedIn</Link>
